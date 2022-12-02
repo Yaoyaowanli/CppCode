f1b3f83647197e1d09d024cc520f862f67c59d35
yaoyuan <907885548@qq.com>
2022-12-02 17:37:30 +0800

    hash_table 开散列、拉链

--- Hash/HashTable.h
+++ Hash/HashTable.h
@@ -131,3 +131,128 @@ namespace yao_close_hash {
         return false;
     }
 }
+
+
+
+namespace yao_open_hash {
+    //HashTable 开散列，哈希桶
+    template<class T>
+    struct HashNode{
+        typedef HashNode<T> Node;
+        T _data;
+        Node* _next;
+        explicit HashNode(const T& data);
+    };
+
+    template<class K,class T,class KOfT>
+    class HashTable{
+    typedef HashNode<T> Node;
+    public:
+        bool Insert(const T& data);
+        Node* Find(const K& key);
+        bool Erase(const K& key);
+    private:
+        std::vector<Node*> _tables;
+        size_t _num = 0;                    //表中存储的数据个数
+    };
+
+
+    template<class T>
+    HashNode<T>::HashNode(const T& data)
+    :_data(data)
+    ,_next(nullptr)
+    {}
+
+
+    template<class K,class T,class KOfT>
+    bool HashTable<K,T,KOfT>::Insert(const T &data) {
+        //开散列当插入数据遇到哈希冲突时，会将冲突数据链接到当前数据后，哈希表中的每一个元素都是一个链表，不过当大量冲突发生时
+        //这些冲突的数据就会挂在同一个链式桶中，链表过长，就会导致查找效率低下；
+        //所以开散列的哈希桶也是需要控制其负载的，我们一般把散列负载因子控制在1。
+        KOfT k_of_t;
+        //检查、扩容
+        if (_tables.size() == _num){
+            std::vector<Node*> new_tables;
+            size_t new_size = _tables.size()==0 ? 10 : _tables.size()*2;
+            new_tables.resize(new_size);
+            for (size_t i=0; i<_tables.size(); i++){
+                //取到每一个桶
+                Node* cur = _tables[i];
+                while (cur){
+                    Node* cur_next = cur->_next;
+                    //计算桶中该元素在新表中的index,链接
+                    size_t index = k_of_t(cur->_data) % new_tables.size();
+                    cur->_next = new_tables[index];
+                    new_tables[index] = cur;
+                    //迭代当前桶
+                    cur = cur_next;
+                }
+                //旧表该位置置空，防止析构节点
+                _tables[i] = nullptr;
+            }
+            _tables.swap(new_tables);
+        }
+        //找这个值是否存在
+        size_t index = k_of_t(data) % _tables.size();
+        Node* cur = _tables[index];
+        while (cur){
+            if (k_of_t(cur->_data) == k_of_t(data)){
+                return false;
+            }else{
+                cur = cur->_next;
+            }
+        }
+        //不存在、插入
+        Node* new_node = new Node(data);
+        new_node->_next = _tables[index];
+        _tables[index] = new_node;
+        ++_num;
+        return true;
+    }
+
+    template<class K,class T,class KOfT>
+    HashNode<T>* HashTable<K,T,KOfT>::Find(const K &key) {
+        KOfT k_of_t;
+        size_t index = key % _tables.size();
+        Node* cur = _tables[index];
+        while (cur){
+            if (k_of_t(cur->_data) == key){
+                return cur;
+            }else{
+                cur = cur->_next;
+            }
+        }
+        return nullptr;
+    }
+
+    template<class K,class T,class KOfT>
+    bool HashTable<K,T,KOfT>::Erase(const K &key) {
+        KOfT k_of_t;
+        size_t index = key % _tables.size();
+        Node* delete_node;
+        Node* cur = _tables[index];
+        //如果index位置就是要删除的数据
+        if (k_of_t(cur->_data) == key){
+            delete_node = cur;    //记录要删除的节点
+            cur = cur->_next;           //cur走到下一个
+            delete delete_node;
+            _tables[index] = cur;       //重新赋值桶首
+            _num--;
+            return true;
+        }
+        while (cur){
+            //如果下一个节点是要删除的节点
+            if (k_of_t((cur->_next)->_data) == key){
+                delete_node = cur->_next;       //记录要删除的节点
+                cur->_next = delete_node->_next;    //cur->next 变为 删除节点的下一个
+                delete delete_node;
+                _num--;
+                return true;
+            }else{
+                cur=cur->_next;
+            }
+        }
+        return false;
+    }
+
+}
